21f7e43b660e980100735e82eecc6f63e484e053
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2023-08-29 02:08:33 +0530

    browser router and routes for pages, fldr strcture

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,22 @@
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
 
+function App() {
   return (
-    <>
-      <h1>hello world</h1>
-    </>
+    <div>
+    <Router>
+    <Routes>
+      <Route path= "/" element={<Home />} />
+      <Route path= "/login" element={<Login />} />
+      <Route path= "/register" element={<Register/>} />
+    </Routes>
+    </Router>
+
+
+    </div>
   )
 }
 
