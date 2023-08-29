e4a1b90231ac37d47663ccf83815b27195e9978e
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2023-08-29 13:47:22 +0530

    settin up api

--- server/server.js
+++ server/server.js
@@ -4,4 +4,4 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`server ok running on port ${port}`))
+app.listen(port, () => console.log(`server running on port ${port}`))
