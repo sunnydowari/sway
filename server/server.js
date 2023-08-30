7ae0eeabeec76a84dfba59202d5e7b112911ec4e
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2023-08-30 13:46:51 +0530

    setting up connection with mongodb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
+require('dotenv').config()
 const app = express();
-
+const dbConfig = require("./config/dbConfig")
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`))
