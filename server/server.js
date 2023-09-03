aaf43f0aee12bb394277ae80c956d549a464d8c3
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2023-09-04 01:15:49 +0530

    server to use routes

--- server/server.js
+++ server/server.js
@@ -4,4 +4,9 @@ const app = express();
 const dbConfig = require("./config/dbConfig")
 const port = process.env.PORT || 5000;
 
+const userRoutes = require("./routes/userRoutes");
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
 app.listen(port, () => console.log(`server running on port ${port}`))
