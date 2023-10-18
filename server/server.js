5aab673c5d2bd6fad3faf232dd1465f49e8142d8
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2023-10-18 15:04:08 +0530

    server bug

--- server/server.js
+++ server/server.js
@@ -17,8 +17,8 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
+    origin: "http://localhost:5173",
+    // methods: ["GET", "POST"],
   },
 });
 
